ce190bf7242088ab1467bf96c9bf1c147cbbedaf
Igor Malyarov <igor1309@gmail.com>
2020-12-19 03:50:28 +0300

    add colors end enum

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "NiceColors",
             dependencies: []),
-        .testTarget(
-            name: "NiceColorsTests",
-            dependencies: ["NiceColors"]),
     ]
 )
--- Sources/NiceColors/NiceColors.swift
+++ Sources/NiceColors/NiceColors.swift
@@ -1,3 +1,131 @@
-struct NiceColors {
-    var text = "Hello, World!"
+//
+//  Colors.swift
+//  OnboardingTesting
+//
+//  Created by Igor Malyarov on 16.12.2020.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+public enum Colors: String, CaseIterable {
+    case brandColor
+
+    /// paul hudson (modified)
+    case darkBlue, lightBlue, purple, midnight, teal, orange, gold, green, pink, red
+
+    /// Trikita Colors http://0xrgb.com
+    case asbesthosConcrete, belizeHolePeterRiver, greenSeaTurquoise, nephritisEmerald
+    case orangeSunFlower, pomegrenadeAlizarin, pumpkinCarrot, silverClouds, wisteriaAmethyst
+
+    /// Material Design https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+    case amber700200, blue700200, blueGray700200, brown700200, cyan700200
+    case deepOrange700200, deepPurple700300, gray700200, green700200
+    case indigo700300, lightBlue700200, lightGreen700200, lime800300
+    case orange700200, pink700200, purple700200, red700200, teal700200, yellow700200
+
+    public var color: Color { Color(rawValue) }
 }
+
+@available(iOS 14.0, *)
+public extension Color {
+    init(_ color: Colors) {
+        self.init(color.rawValue)
+    }
+}
+
+/*
+ https://github.com/trikita/palette
+ GreenSeaTurquoise
+ #16a085
+ #1abc9c
+
+ NephritisEmerald
+ #27ae60
+ #2ecc71
+
+ BelizeHolePeterRiver
+ #2980b9
+ #3498db
+
+ WisteriaAmethyst
+ #8e44ad
+ #9b59b6
+
+ MidnightBlueWetAsphalt
+ #2c3e50
+ #34495e
+
+ OrangeSunFlower
+ #f39c12
+ #f1c40f
+
+ PumpkinCarrot
+ #d35400
+ #e67e22
+
+ PomegrenadeAlizarin
+ #c0392b
+ #e74c3c
+
+ SilverClouds
+ #bdc3c7
+ #ecf0f1
+
+ AsbesthosConcrete
+ #7f8c8d
+ #95a5a6
+ */
+
+/*
+ https://github.com/trikita/palette
+ turquoise
+ #1abc9c
+ green sea
+ #16a085
+
+ emerald
+ #2ecc71
+ nephritis
+ #27ae60
+
+ peter river
+ #3498db
+ belize hole
+ #2980b9
+
+ amethyst
+ #9b59b6
+ wisteria
+ #8e44ad
+
+ wet asphalt
+ #34495e
+ midnight blue
+ #2c3e50
+
+ sun flower
+ #f1c40f
+ orange
+ #f39c12
+
+ carrot
+ #e67e22
+ pumpkin
+ #d35400
+
+ alizarin
+ #e74c3c
+ pomegrenade
+ #c0392b
+
+ clouds
+ #ecf0f1
+ silver
+ #bdc3c7
+
+ concrete
+ #95a5a6
+ asbesthos
+ #7f8c8d
+ */
